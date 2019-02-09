235dc889b7aeb92492fc62e89b378a33515461a4
Peter Feerick <peter.feerick@gmail.com>
2019-02-09 14:41:35 +1000

    Draft of data structs for modbus register data

--- src/main.cpp
+++ src/main.cpp
@@ -26,6 +26,130 @@ float stats_today_pv_volt_min, stats_today_pv_volt_max;
 uint8_t result;
 
 // print "Version:", repr(response.information[2])
+//rated data
+struct rated_data
+{
+  float pv_voltage;
+  float pv_current;
+  float pw_power;
+  float batt_voltage;
+  float batt_current;
+  float batt_power;
+  float charging_mode; //0000H Connect/disconnect, 0001H PWM, 0002H MPPT
+  float load_current;
+} ratedData;
+
+//realtime status
+struct realtime_status
+{
+  float pv_voltage;
+  float pv_current;
+  float pv_power;
+  float batt_voltage;
+  float batt_current;
+  float batt_power;
+  float load_voltage;
+  float load_current;
+  float load_power;
+  float batt_temp;
+  float equip_temp;
+  float heatsink_temp;
+  float battery_soc;
+  float battery_remote_temp;
+  float battery_rated_power; //1200,2400 for 12/12v
+} realtimeStatus;
+
+//statistical parameters
+struct statistical_parameters
+{
+  float todayMaxPvVoltage;
+  float todayMinPvVoltage;
+  float todayMaxBattVoltage;
+  float todayMinBattVoltage;
+  float todayConsumedEnergy;
+  float monthConsumedEnergy;
+  float yearConsumedEnergy;
+  float totalConsumedEnergy;
+  float todayGeneratedEnergy;
+  float monthGeneratedEnergy;
+  float yearGeneratedEnergy;
+  float totalGeneratedEnergy;
+  float CO2reduction;
+  float batteryCurrent; //net, charging minus discharge
+  float batteryTemp;
+  float ambientTemp;
+} statisticalParameters;
+
+//setting_parameters
+struct setting_parameters
+{
+  float batteryType;
+  float ratedCapacity;
+  int temperatureCompensation;
+  float highVoltageDisconnect;
+  float chargeLimitVoltage;
+  float overVoltageReconnect;
+  float equalisationVoltage;
+  float boostVoltage;
+  float floatVoltage;
+  float boostReconnectVoltage;
+  float lowVoltageReconnect;
+  float underVoltageRecover;
+  float lowVoltageDisconnect;
+  float realTimeClock;
+  int equalisationChargingCycle;
+  float batteryTemperatureWarningUpperLimit;
+  float batteryTemperatureWarningLowerLimit;
+  float controllerInnerTemperatureUpperLimit;
+  float controllerInnerTemperatureUpperLimitRecovery;
+  float powerComponentTemperatureUpperLimit;
+  float powerComponentTemperatureUpperLimitRecover;
+  float lineImpedance;
+  float nightTimeThresholdVoltage;
+  float lightSignalStartupDelayTime;
+  float dayTimeThresholdVoltage;
+  float lightSignalTurnOffDelayTime;
+  float loadControllingMode;
+  float workingTimeLength1;
+  float workingTimeLength2;
+  int turnOnTiming1_H;
+  int turnOnTiming1_M;
+  int turnOnTiming1_S;
+  int turnOffTiming1_H;
+  int turnOffTiming1_M;
+  int turnOffTiming1_S;
+  int turnOnTiming2_H;
+  int turnOnTiming2_M;
+  int turnOnTiming2_S;
+  int turnOffTiming2_H;
+  int turnOffTiming2_M;
+  int turnOffTiming2_S;
+  float lengthOfNight;
+  int batteryRatedVoltageCode;       //0=auto, 1=12V, 2=24V
+  bool loadTimingControl;            //0=1 timer,1=2 timer
+  bool defaultLoadOnOffInManualMode; //0=OFF, 1=ON
+  int equaliseDuration;              //minute
+  int boostDuration;                 //minute
+  int dischargingPercentage;         //$
+  int chargingPercentage;            //%
+  bool batteryManagementMode;        //0=voltComp, 1=SoC
+} settingParameters;
+
+//coil / switch values
+struct switch_value
+{
+  bool manualControl;
+  bool loadTest;
+  bool forceLoad;
+} switchValues;
+
+//discrete_input
+struct discrete_input
+{
+  bool overTemp;
+  bool dayNight;
+} discreteInput;
+
 bool rs485DataReceived = true;
 bool loadPoweredOn = true;
 
